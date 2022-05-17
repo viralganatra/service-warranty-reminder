90b5a4ced47e55d9349e27bc9288547d6aaa1476
Viral Ganatra <viral@viralganatra.com>
2022-05-17 16:56:11 +0100

    Add GitHub workflow :white_check_mark:

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  modulePathIgnorePatterns: ['<rootDir>/build/'],
   preset: 'ts-jest',
   setupFiles: ['./config/jest-setup-files.js'],
   testEnvironment: 'node',

4fafaaf1fba67f0a9b5ea8142e4e1b1dd2e58da3
Viral Ganatra <viral@viralganatra.com>
2022-05-19 18:43:48 +0100

    Fix file formatting :rotating_light:

--- src/utils/__mocks__/ssm.ts
+++ src/utils/__mocks__/ssm.ts
@@ -1,5 +1,5 @@
 export default function getSSMParams(params: string[]) {
-  const map: Record<string, { Name: string, Value: string }> = {};
+  const map: Record<string, { Name: string; Value: string }> = {};
 
   for (const param of params) {
     map[param] = { Name: param, Value: param };

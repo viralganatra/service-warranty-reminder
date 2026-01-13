b983bf0b5d1fec38ec98f39c30bf0dd97820a0be
Viral Ganatra <viral@viralganatra.com>
2026-01-13 09:39:44 +0000

    Remove unused flag from validate script

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -6,5 +6,5 @@ pnpm concurrently \
   --names "test,lint,typecheck" \
   --prefix-colors "bgRed.bold.white,bgGreen.bold.white,bgBlue.bold.white,bgMagenta.bold.white" \
     "pnpm test --silent -- --watch=false" \
-    "pnpm lint --quiet" \
+    "pnpm lint" \
     "pnpm typecheck"
